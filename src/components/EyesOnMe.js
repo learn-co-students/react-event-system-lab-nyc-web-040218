5612500e46fa707c24fda26aac7e0d980e60c564
yongnicholaskim <yongnicholaskim@gmail.com>
2018-06-04 23:08:36 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onFocus = () => {
+    console.log("Good!");
+  };
+
+  onBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  render() {
+    return (
+      <button
+        onFocus={this.onFocus}
+        onBlur={this.onBlur}
+        >Yo!
+      </button >
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,28 @@
 // Code Keypad Component Here
+
+import React, { Component } from "react";
+// import React from "react";
+
+// class Keypad extends React.Component {
+class Keypad extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  enteringPassword = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          onKeyUp={this.enteringPassword}
+        />
+      </div>
+    )
+  }
+}
+
+export default Keypad;
