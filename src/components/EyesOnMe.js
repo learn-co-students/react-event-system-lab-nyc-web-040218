5aabe1bbcb774a0bbd235cbdac06afc4551d3fec
Matthew Talbot <mdtalbot@gmail.com>
2018-06-25 17:39:17 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,31 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default class EyesOnMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  focusHandler = ()=> {
+    console.log('Good!')
+  }
+
+  blurHandler() {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focusHandler} onBlur={this.blurHandler} />
+    );
+  }
+}
+
+ EyesOnMe.propTypes = propTypes;
+ EyesOnMe.defaultProps = defaultProps;
